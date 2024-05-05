15ac8e1df6127861ddfdbe43bbd1e41eb5587225
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-05 11:10:14 +0200

    Update selection merveille

--- jetonProgres.h
+++ jetonProgres.h
@@ -1,8 +1,5 @@
 #ifndef LO21_P24_JETONPROGRES_H
 #define LO21_P24_JETONPROGRES_H
-#include "joueur.h"
-#include "plateauJeu.h"
-#include "batiment.h"
 #include <iostream>
 #include <string>
 
@@ -11,7 +8,9 @@ private:
     std::string nomJeton;
     std::string effet;
 public:
-    JetonProgres(std::string n, std::string e) : nomJeton(std::move(n)), effet(std::move(e)) {};
+    JetonProgres()=default;
+    JetonProgres(std::string n, std::string e) : nomJeton(std::move(n)), effet(std::move(e)) {}
+    ~JetonProgres()=default;
     std::string getNomJeton() const {return nomJeton;}
     std::string getEffetJeton() const {return effet;}
 };

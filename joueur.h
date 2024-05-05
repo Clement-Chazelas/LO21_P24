3b6b4ed694fd179c2331727a05e922122055657b
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-05 11:12:14 +0200

    Update selection merveille

--- joueur.h
+++ joueur.h
@@ -2,36 +2,37 @@
 #define JOUEUR_H
 #include <iostream>
 #include <string>
+#include "cartes.h"
 //#include "jetonProgres.h"         Activer cette ligne fait buguer la même qui est dans plateauJeu
 //#include "plateauJeu.h"
-#include "batiment.h"
 
-
-using namespace std;
 
 class Joueur {
 private:
-    string nom;
-    string prenom;
+    Merveille* merveilles;
+    std::string nom;
+    std::string prenom;
     unsigned int nbPieces;
     /*
     Batiment* cite=nullptr;
     JetonProgres* jetonsScientifiques=nullptr;
-    Merveille* merveilles=nullptr;
     */
+    unsigned int nbMerveilles;
 public:
-    Joueur(string n, string p, unsigned int m=7) : nom(n), prenom(p), nbPieces(m) {}
-    //~Joueur() {delete[] cite; delete[] jetonsScientifiques; delete[] merveilles;}
+    Joueur(std::string n, std::string p, unsigned int m=7) :  merveilles(new Merveille[4]), nom(n), prenom(p), nbPieces(m), nbMerveilles(0) {}
+    ~Joueur() {/*delete[] merveilles;*/} //Je fais volontairement une fuite memoire car je n'ai pas trouve pourquoi la suppression du tableau merveille fais crasher le programme
     Joueur(const Joueur& j)=delete;
-
-    string getNom() const {return nom;}
-    string getPrenom() const {return prenom;}
+    std::string getNom() const {return nom;}
+    std::string getPrenom() const {return prenom;}
     unsigned int getnbPieces() const {return nbPieces;}
 
     void gainPieces(unsigned int n) {nbPieces+=n;}
     void pertePieces(unsigned int n) {if (n>nbPieces) nbPieces=0; else nbPieces-=n;}
+
+    void ajouterMerveille(Merveille& mer);
+
 };
 
-ostream& operator<<(ostream& f, const Joueur& j);
+std::ostream& operator<<(std::ostream& f, const Joueur& j);
 
 #endif // JOUEUR_H

629805d5d03c802f396c77bb5a42099a14a9f0a8
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-05 11:10:47 +0200

    Update selection merveille

--- joueur.cpp
+++ joueur.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
 #include "joueur.h"
+#include "plateauJeu.h"
 
 using namespace std;
+
+
+void Joueur::ajouterMerveille(Merveille& mer) {
+    merveilles[nbMerveilles++] = mer;
+}
+
 
 ostream& operator<<(ostream& f, const Joueur& j) {
     f << "Joueur : " << j.getPrenom() << " " << j.getNom();
     return f;
 }
-

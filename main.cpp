51a2548e4d9632831e0cf46bf30232295f3d2930
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-09 18:51:17 +0200

    Generation Age I

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
+#include <QApplication>
+#include <QString>
 #include <iostream>
-#include "partie.h"
 #include <string>
+#include "partie.h"
 #include "utils.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    QApplication a(argc, argv);
 
     Partie p("Ducasse", "Agathe", "Chazelas", "Clement");
     cout << p << endl;
 
-    p.genererHuitMerveilles(); //generation random parmis un csv
+    //p.genererHuitMerveilles(); //generation random parmis un csv
 
-    cout << p.getPlateau(); //on affiche les merveilles qu'il y a dans le plateau de jeu
-    // il faudra bien sur completer l'affichage du plateau au fur et a mesure
+    p.genererAgeUn(); //idem
+    //Pour l'instant les batiments sont simplement dans une liste de pointeur de plateau de jeu
+
+    //p.selectionDesMerveilles();
+
+    cout << p.getPlateau(); //on affiche les merveilles et les batiments qu'il y a sur le plateau de jeu
 
     return 0;
 }
-
-}
-

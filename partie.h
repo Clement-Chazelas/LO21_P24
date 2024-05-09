0bd0431e12473635b5d15d25e5fa8fedc57d8dbd
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-09 18:52:34 +0200

    Generation Age I

--- partie.h
+++ partie.h
@@ -15,10 +15,15 @@ class Partie
 public:
     Partie(const std::string& n_j1, const std::string& p_j1, const std::string& n_j2, const std::string& p_j2);
     ~Partie()=default;
+
+    //getters
     const Joueur& getJoueur1() const {return joueur1;}
     const Joueur& getJoueur2() const {return joueur2;}
     const PlateauDeJeu& getPlateau() const {return plateau;}
+
     void genererHuitMerveilles();
+    void genererAgeUn();
+    void afficherMerveillesRestantes(bool salve2=false) const;
     void selectionDesMerveilles();
 };
 

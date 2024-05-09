c21ebc1e061c34470f5715274c0514f930ceb3c2
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-09 18:43:36 +0200

    Generation Age I

--- cartes.cpp
+++ cartes.cpp
@@ -14,44 +14,56 @@ void Carte::ajouterCoutRessources(Ressources r) {
     coutRessources[nbCout++] = r;
 }
 
-
 Merveille::Merveille(const Merveille& mer) :
-    Carte(mer.getNom(), mer.getNbPointVictoire()), active(mer.getActive()), rejouer(mer.getRejouer()),
-    produitRessources(new Ressources[mer.getNbCout()]), nbProd(mer.getNbProd()),
+    Carte(mer), active(mer.getActive()), rejouer(mer.getRejouer()),
+    produitRessources(new Ressources[mer.getNbProdMax()]), nbProd(mer.getNbProd()),
     nbProdMax(mer.getNbProdMax()), nbPointsCombat(mer.getNbPointsCombat()),
     nbPiecesRapportees(mer.getNbPiecesRapportees()), nbPiecesSacagees(mer.getNbPiecesSacagees()) {
         for (unsigned int i=0; i<nbCout; i++) coutRessources[i]=mer.getCoutRessources()[i];
         for (unsigned int i=0; i<nbProd; i++) produitRessources[i]=mer.getProduitRessources()[i];
 }
 
+Batiment::Batiment(const Batiment& bat) :
+    Carte(bat), coutPieces(bat.getCoutPieces()), coutChainage(bat.getCoutChainage()),
+    symboleChainage(bat.getSymboleChainage()), faceCachee(bat.getFaceCachee()) {}
 
-string printRessource(const Ressources& r) {
-    switch (r) {
-    case Ressources::bois:
-        return "bois";
-        break;
-    case Ressources::pierre:
-        return "pierre";
-        break;
-    case Ressources::argile:
-        return "brique";
-        break;
-    case Ressources::papyrus:
-        return "papyrus";
-        break;
-    case Ressources::verre:
-        return "verre";
-        break;
-    }
-    return "inexistant";
+BatimentRessource::BatimentRessource(const BatimentRessource& other) :
+    Batiment(other), ressourceProduite(other.getRessourceProduite()), nbRessourcesProduites(other.getNbRessourcesProduites()) {}
+
+BatimentCivil::BatimentCivil(const BatimentCivil& other) :
+    Batiment(other) {}
+
+BatimentScientifique::BatimentScientifique(const BatimentScientifique& other) :
+    Batiment(other), symbole(other.getSymbole()) {}
+
+BatimentCommercial::BatimentCommercial(const BatimentCommercial& other) :
+    Batiment(other), piecesRapportees(other.getPiecesRapportees()), typePourGainPieces(other.getTypePourGainPiece()),
+        nbRessourcesStockees(other.getNbRessourcesStockees()), nbRessourcesDisponibles(other.getNbRessourcesDisponibles()),
+        ressourcesStockees(new Ressources[other.getNbRessourcesStockees()]), ressourcesDisponibles(new Ressources[other.getNbRessourcesDisponibles()]) {
+    for (unsigned int i=0; i<nbRessourcesStockees; i++) ressourcesStockees[i]=other.getRessourcesStockees()[i];
+    for (unsigned int i=0; i<nbRessourcesDisponibles; i++) ressourcesDisponibles[i]=other.getRessourcesDisponibles()[i];
 }
 
+BatimentMilitaire::BatimentMilitaire(const BatimentMilitaire& other) :
+    Batiment(other), pointsCombats(other.getNbPointsCombats()) {}
+
+
+Carte& Carte::operator=(const Carte& other) {
+    if (this!=&other) {
+        nom=other.getNom();
+        nbCout=other.getNbCout();
+        nbCoutMax=other.getNbCoutMax();
+        nbPointVictoire= other.getNbPointVictoire();
+        delete[] coutRessources;
+        coutRessources=new Ressources[nbCoutMax];
+        for (unsigned int i=0; i<nbCout; i++) coutRessources[i]=other.getCoutRessources()[i];
+    }
+    return *this;
+}
 
 Merveille& Merveille::operator=(const Merveille& mer) {
     if (this!=&mer) {
-        nom=mer.getNom();
-        nbCout=mer.getNbCout();
-        nbPointVictoire= mer.getNbPointVictoire();
+        Carte::operator=(mer);
         nbPointsCombat=mer.getNbPointsCombat();
         active=mer.getActive();
         rejouer=mer.getRejouer();
@@ -59,15 +71,70 @@ Merveille& Merveille::operator=(const Merveille& mer) {
         nbProdMax=mer.getNbProdMax();
         nbPiecesRapportees=mer.getNbPiecesRapportees();
         nbPiecesSacagees=mer.getNbPiecesSacagees();
-        for (unsigned int i=0; i<nbCout; i++) {
-            coutRessources[i]=mer.getCoutRessources()[i];
-        }
         for (unsigned int i=0; i<nbProd; i++) produitRessources[i]=mer.getProduitRessources()[i];
     }
     return *this;
 }
 
 
+Batiment& Batiment::operator=(const Batiment& other) {
+    if (this!=&other) {
+        Carte::operator=(other);
+        coutPieces=other.getCoutPieces();
+        coutChainage=other.getCoutChainage();
+        symboleChainage=other.getSymboleChainage();
+        faceCachee=other.getFaceCachee();
+    }
+    return *this;
+}
+
+BatimentRessource& BatimentRessource::operator=(const BatimentRessource& other) {
+    if (this!=&other) {
+        Batiment::operator=(other);
+        ressourceProduite=other.getRessourceProduite();
+        nbRessourcesProduites=other.getNbRessourcesProduites();
+    }
+    return *this;
+}
+
+BatimentCivil& BatimentCivil::operator=(const BatimentCivil& other) {
+    if (this!=&other) {
+        Batiment::operator=(other);
+    }
+    return *this;
+}
+
+BatimentScientifique& BatimentScientifique::operator=(const BatimentScientifique& other) {
+    if (this!=&other) {
+        Batiment::operator=(other);
+        symbole=other.getSymbole();
+    }
+    return *this;
+}
+
+BatimentCommercial& BatimentCommercial::operator=(const BatimentCommercial& other) {
+    if (this!=&other) {
+        Batiment::operator=(other);
+        piecesRapportees=other.getPiecesRapportees();
+        typePourGainPieces=other.getTypePourGainPiece();
+        nbRessourcesStockees=other.getNbRessourcesStockees();
+        nbRessourcesDisponibles=other.getNbRessourcesDisponibles();
+        delete[] ressourcesStockees; delete[] ressourcesDisponibles;
+        ressourcesStockees=new Ressources[nbRessourcesStockees];
+        ressourcesDisponibles=new Ressources[nbRessourcesDisponibles];
+        for (unsigned int i=0; i<nbRessourcesStockees; i++) ressourcesStockees[i]=other.getRessourcesStockees()[i];
+        for (unsigned int i=0; i<nbRessourcesDisponibles; i++) ressourcesDisponibles[i]=other.getRessourcesDisponibles()[i];
+    }
+    return *this;
+}
+
+BatimentMilitaire& BatimentMilitaire::operator=(const BatimentMilitaire& other) {
+    if (this!=&other) {
+        Batiment::operator=(other);
+        pointsCombats=other.getNbPointsCombats();
+    }
+    return *this;
+}
 
 
 void Merveille::ajouterProduitRessources(Ressources r){
@@ -81,10 +148,15 @@ void Merveille::ajouterProduitRessources(Ressources r){
     produitRessources[nbProd++] = r;
 }
 
+void BatimentCommercial::AjouterRessourcesStockees(Ressources r) {
+    ressourcesStockees[nbRessourcesStockees++]=r;
+}
+
 
 ostream& operator<<(ostream& f, const Merveille& m) {
-    f << "Merveille : " << m.getNom() << std::endl;
-    f << "cout : ";
+    f << "___Merveille : " << m.getNom() << "___";
+    if (!m.getActive()) f << " (inactive)";
+    f << "\ncout : ";
     for (unsigned int i=0; i<m.getNbCout(); i++) {
         f << "# " << printRessource(m.getCoutRessources()[i]) << "    ";
     }
@@ -104,5 +176,16 @@ ostream& operator<<(ostream& f, const Merveille& m) {
         f << std::endl << "Cette merveille a l'effet rejouer";
     }
     return f << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& f, const Batiment& bat) {
+    bat.afficher(f);
+    f << "Points de victoire: " << bat.getNbPointVictoire() << " - ";
+    f << "Cout en pieces: " << bat.getCoutPieces() << "   ";
+    if (bat.getCoutChainage()!="NULL") f << "Chainage requis: " << bat.getCoutChainage() << "   ";
+    if (bat.getSymboleChainage()!="NULL") f << "Symbole de chainage: " << bat.getSymboleChainage() << "   ";
+    if (!bat.getFaceCachee()) f << "Face cachee: " << (bat.getFaceCachee() ? "Oui" : "Non");
+    f << "\n" << std::endl;
+    return f;
 }
 

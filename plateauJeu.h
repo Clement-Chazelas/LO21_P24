0ef9d09f5718a8a496ab0d90ab805cb28dfac9f7
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-09 18:53:48 +0200

    Generation Age I

--- plateauJeu.h
+++ plateauJeu.h
@@ -9,11 +9,11 @@
 class PlateauDeJeu {
 private:
     Batiment** defausse;
-    Batiment* tab;
+    Batiment** tab; //Les batiments sont de tailles differents => il faut passer par un tableau de pointeur
     JetonProgres* piocheJeton;
     JetonProgres* plateauJeton;
     Merveille* selectionMerveille;
-    size_t nb_cartes;
+    size_t nb_batiment_plateau;
     size_t nb_jeton_pioche;
     size_t nb_jeton_plateau;
     size_t nb_defausse;
@@ -23,24 +23,39 @@ private:
 public:
     PlateauDeJeu(size_t c, size_t pio, size_t pla);
     ~PlateauDeJeu() {delete[] defausse; delete[] tab; delete[] piocheJeton; delete[] plateauJeton; delete[] selectionMerveille;}
-    void setDispositionCartes(size_t i);
-    void setJetonProgresPioche(size_t i);
-    void setJetonProgresPlateau(size_t i);
-    Batiment* getDispositionCarte() const {return tab;}
+
+    //getters
+    Batiment** getBat() const {return tab;}
+    Batiment** getDispositionCarte() const {return tab;}
     JetonProgres* getJetonProgresPioche() const {return piocheJeton;}
     JetonProgres* getJetonProgresPlateau() const {return plateauJeton;}
     unsigned int getNb_merveille_plateau() const {return nb_merveille_plateau;}
+    unsigned int getNb_batiment_plateau() const {return nb_batiment_plateau;}
     Merveille* getSelectionMerveille() const {return selectionMerveille;}
+    Batiment* getDefausse() {return *defausse;}
+
+    //setters
+    void setDispositionCartes(size_t i);
+    void setJetonProgresPioche(size_t i);
+    void setJetonProgresPlateau(size_t i);
+    void setDefausse(Batiment& bat);      //Défausser
+    void setPionMilitaire(int i) {emplacementPionMilitaire += i ;if(emplacementPionMilitaire >= 10 || emplacementPionMilitaire <= -10){/*DECLENCHER_FIN_PARTIE*/};}     //Remplacerr le déclenchement par la méthode de fin
+
+    //Pour les merveilles
+    Merveille prendreMerveillePlateau(size_t i);
+    Merveille choisirMerveille(bool j2=false);
+    void melangerMerveilles();
+    void ajouterMerveillePlateau(Merveille& mer);
+
+    //Pour les batiments
     void prendreCarte(size_t i);
+    void ajouterBatimentPlateau(Batiment* bat);
+    Batiment* piocher(int i);
+    void melangerBatiments();
+
+    //Pour les jetons
     void prendreJetonPioche(size_t i);
     void prendreJetonPlateau(size_t i);
-    Merveille prendreMerveillePlateau(size_t i);
-    Batiment* piocher(int i);
-    void setDefausse(Batiment& bat);      //Défausser
-    Batiment* getDefausse() {return *defausse;}
-    void ajouterMerveillePlateau(Merveille& mer);
-    Merveille& choisirMerveille();
-    void setPionMilitaire(int i) {emplacementPionMilitaire += i ;if(emplacementPionMilitaire >= 10 || emplacementPionMilitaire <= -10){/*DECLENCHER_FIN_PARTIE*/};}     //Remplacerr le déclenchement par la méthode de fin
 };
 
 std::ostream& operator<<(std::ostream& f, const PlateauDeJeu& pla);

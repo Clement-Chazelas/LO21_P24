69c40e63abadd504720577ae5931d3af47c3e095
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-10 23:00:59 +0200

    Gen Age II

--- joueur.cpp
+++ joueur.cpp
@@ -9,9 +9,17 @@ void Joueur::ajouterMerveille(const Merveille& mer) {
 }
 
 
+void Joueur::ajouterBatiment(Batiment* bat) {
+    std::cout << "youhou" << std::endl;
+    cite[nbBatiments++]=bat; //Pas necessaire pour l'instant de gerer le nombre de merveilles du joueur (tjs = 4)
+}
+
+
 ostream& operator<<(ostream& f, const Joueur& j) {
     f << "===Joueur : " << j.getPrenom() << " " << j.getNom() << "===" << endl;
     f << "Merveilles dans sa cite : " << endl;
     for (unsigned int i=0; i<j.getNbMerveilles(); i++) f << j.getMerveilles()[i];
+    f << "Batiments dans sa cite : " << endl;
+    for (unsigned int i=0; i<j.getNbBatiments(); i++) f << *(j.getCite()[i]);
     return f;
 }

fc37d8189b188dc21f0b8dfcf892480f08c0aaa3
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-10 23:03:29 +0200

    Gen Age II

--- main.cpp
+++ main.cpp
@@ -16,12 +16,18 @@ int main(int argc, char *argv[]) {
 
     //p.genererHuitMerveilles(); //generation random parmis un csv
 
-    p.genererAgeUn(); //idem
-    //Pour l'instant les batiments sont simplement dans une liste de pointeur de plateau de jeu
+    //p.genererAgeUn(); //idem pour les batiments de l'age I
+    //p.genererAgeDeux(); // Age I et Age II ne peuvent pas etre genere en meme temps pour le moment
 
-    //p.selectionDesMerveilles();
+    //p.selectionDesMerveilles(); //Choix successifs des merveilles
+    //Il faut mettre le nom exact pour choisir la carte !
 
-    cout << p.getPlateau(); //on affiche les merveilles et les batiments qu'il y a sur le plateau de jeu
+    //p.getPlateau().genererStructureAge1(); Permet de generer la tableau statique (dim 2) pour la structure des cartes
+
+    //p.selectionDesBatiments(); Uniquement si la gen de l'age I a ete effectue
+
+    //cout << p.getPlateau(); //afficher les merveilles et les batiments qu'il y a sur le plateau de jeu
 
     return 0;
 }
+

2eab32262cd84b37eab373e40432e04c771fb5f3
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-16 11:13:50 +0200

    Update to do list

--- main.cpp
+++ main.cpp
@@ -14,22 +14,39 @@ int main(int argc, char *argv[]) {
     Partie p("Ducasse", "Agathe", "Chazelas", "Clement");
     cout << p << endl;
 
-    //p.genererHuitMerveilles(); //generation random parmis un csv
+    /*
+    p.genererHuitMerveilles(); //generation random parmis un csv pour choisir 8 merveilles
+    p.selectionDesMerveilles(); //Choix successifs des merveilles
+    */
 
-    //p.genererAgeUn(); //idem pour les batiments de l'age I
-    //p.genererAgeDeux(); // Age I et Age II ne peuvent pas etre genere en meme temps pour le moment
+    p.genererAgeUn(); //generation random parmis un csv pour exclure 3 batiments
+    p.getPlateau().genererStructureAge1(); //generation de la structure plateau des batiments de l'age 1
+    p.selectionDesBatiments(); //Choix successifs des batiments
 
-    //p.selectionDesMerveilles(); //Choix successifs des merveilles
-    //Il faut mettre le nom exact pour choisir la carte !
+    /*
+    p.genererAgeDeux();
+    p.getPlateau().genererStructureAge2();
+    p.selectionDesBatiments();
 
-    //p.getPlateau().genererStructureAge1(); Permet de generer la tableau statique (dim 2) pour la structure des cartes
-
-    //p.selectionDesBatiments(); Uniquement si la gen de l'age I a ete effectue
-
-    //cout << p.getPlateau(); //afficher les merveilles et les batiments qu'il y a sur le plateau de jeu
+    p.genererAgeTrois();
+    p.getPlateau().genererStructureAge3();
+    p.selectionDesBatiments();
+    */
 
     return 0;
 }
+
+/*
+To Do list :
+    - Generation aleatoire des jetons scientifique sur et hors plateau
+    - Finaliser l'ajout des attributs spéciaux des merveilles (ressources disponibles, choix du jeton, choix dans la defausse...
+    - Gestion du conflit militaire
+    - Assigner la gestion du cycle de vie de tous les objets chacun à une classe
+    - Nettoyer le code (retirer les methodes inutilises, nettoyer la memoire pendant la partie, commencer à mettre des commentaires pour les méthodes un peu complexes)
+    - faire une méthode dans la classe partie pour faire derouler l'integralite de la partie
+    - Photocopier les cartes du jeu
+    - S'attaquer à la partie graphique...
+*/
 
 /*
 To Do list :

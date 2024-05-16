0b041b99a7eadf30b8df9387224a2ad0634dd49d
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-16 12:51:57 +0200

    Choix action joueur

--- plateauJeu.cpp
+++ plateauJeu.cpp
@@ -14,7 +14,7 @@ PlateauDeJeu::PlateauDeJeu(size_t c, size_t pio, size_t pla)
     defausse(new Batiment*[c]), tab(new Batiment*[c]),
     piocheJeton(new JetonProgres[pio]), plateauJeton(new JetonProgres[pla]),
     selectionMerveille(new Merveille[8]), emplacementPionMilitaire(0), nb_merveille_plateau(0) {
-    for (size_t i = 0; i < 7; ++i) {
+    for (size_t i = 0; i < 8; ++i) {
         for (size_t j = 0; j < 13; ++j) {
             structure[i][j]=nullptr;
         }
@@ -72,12 +72,12 @@ void PlateauDeJeu::ajouterBatimentPlateau(Batiment* bat) {
 
 Merveille PlateauDeJeu::prendreMerveillePlateau(size_t i) {
     if(i >= nb_merveille_plateau || i>4){
-        throw "Erreur : cette merveille n'est pas sur le plateau"; //le choix des merveilles se fait de 4 en 4 donc on a sur le plateau au plus 4 merveilles à ce moment
+        throw "Erreur : cette merveille n'est pas sur le plateau";
     }
     else{
         Merveille merveilleChoisie(selectionMerveille[i]);
         for(i; i < 4; i++){
-            selectionMerveille[i] = selectionMerveille[i+1]; //on supprime la merveille dans la liste des merveilles que l'on peut selectionner car elle a été choisie par un joueur
+            selectionMerveille[i] = selectionMerveille[i+1];
         }
         nb_merveille_plateau--;
         return merveilleChoisie;
@@ -87,9 +87,9 @@ Merveille PlateauDeJeu::prendreMerveillePlateau(size_t i) {
 
 void PlateauDeJeu::melangerMerveilles() {
     // Initialisation du générateur de nombres aléatoires
-    std::srand(std::time(nullptr)); //permet une graine différente à chaque fois
+    std::srand(std::time(nullptr));
     for (unsigned int i=nb_merveille_plateau-1; i>0; i--) {
-        unsigned int j=std::rand()%(i+1); //génère un nb aléatoire dans la plage 0 à i inclu
+        unsigned int j=std::rand()%(i+1);
         if (i!=j) {
             Merveille temp(selectionMerveille[i]);
             selectionMerveille[i]=selectionMerveille[j];
@@ -112,27 +112,75 @@ void PlateauDeJeu::melangerBatiments() {
 }
 
 void PlateauDeJeu::genererStructureAge1() {
-    bool modele[7][13]={
-                        {false, false, false, false, false, true, false, true, false, false, false, false, false},
-                        {false, false, false, false, true, false, true, false, true, false, false, false, false},
-                        {false, false,false, true, false, true, false, true, false, true, false, false, false,},
-                        {false, false,true, false, true, false, true, false, true, false, true, false,false},
-                        {false, true, false, true, false, true, false, true, false, true, false, true, false},
-                        {false, false, false, false, false, false, false, false, false, false, false, false, false},
-                        {false, false, false, false, false, false, false, false, false, false, false, false, false}
-                                                                                              };
+    bool modele[8][13]={
+        {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
     unsigned int nbBatInseres=0;
     for (unsigned int i=0; i<5; i++) {
         for (unsigned int j=1; j<12; j++) {
             if (modele[i][j]) {
-                structure[i][j]=tab[nbBatInseres++];
+                structure[i][j]=tab[nbBatInseres];
+                tab[nbBatInseres++]=nullptr;
             }
         }
     }
 }
 
-void PlateauDeJeu::afficherStructureAge1() {
+void PlateauDeJeu::genererStructureAge2() {
+    bool modele[8][13]={
+        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    unsigned int nbBatInseres=0;
     for (unsigned int i=0; i<5; i++) {
+        for (unsigned int j=1; j<12; j++) {
+            if (modele[i][j]) {
+                structure[i][j]=tab[nbBatInseres];
+                tab[nbBatInseres++]=nullptr;
+            }
+        }
+    }
+}
+
+
+void PlateauDeJeu::genererStructureAge3() {
+    bool modele[8][13]={
+        {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    unsigned int nbBatInseres=0;
+    for (unsigned int i=0; i<5; i++) {
+        for (unsigned int j=1; j<12; j++) {
+            if (modele[i][j]) {
+                structure[i][j]=tab[nbBatInseres];
+                tab[nbBatInseres++]=nullptr;
+            }
+        }
+    }
+}
+
+void PlateauDeJeu::afficherStructure() {
+    std::cout << "\n ____________Batiments dans la structure : _________\n";
+    for (unsigned int i=0; i<7; i++) {
         for (unsigned int j=1; j<12; j++) {
             if (structure[i][j]!=nullptr) {
                 std::cout << *(structure[i][j]);
@@ -142,10 +190,10 @@ void PlateauDeJeu::afficherStructureAge1() {
     }
 }
 
-void PlateauDeJeu::afficherSelectionnableAge1() {
+void PlateauDeJeu::afficherSelectionnable() {
     std::cout << "\n ____________Batiments a selectionner : _________\n";
-    for (unsigned int i=0; i<5; i++) {
-        for (unsigned int j=1; j<12; j++) {
+    for (unsigned int i=0; i<8; i++) {
+        for (unsigned int j=1; j<13; j++) {
             if (structure[i][j]!=nullptr && structure[i+1][j-1]==nullptr && structure[i+1][j+1]==nullptr) {
                 std::cout << i << " " << j << " " << *(structure[i][j]);
             }
@@ -155,12 +203,10 @@ void PlateauDeJeu::afficherSelectionnableAge1() {
 
 
 Batiment* PlateauDeJeu::choisirBatiment(bool j2) {
-    bool ok2;
-    QString windowJoueur;
-    if (j2) windowJoueur="Joueur 2";
-    else windowJoueur="Joueur 1";
-    QString batimentChoisie = QInputDialog::getText(nullptr, windowJoueur, "Batiment choisie :", QLineEdit::Normal, "", &ok2);
-    for (unsigned int i=0; i<5; i++) {
+    bool ok1;
+    QString windowJoueur = j2 ? "Joueur 2" : "Joueur 1";
+    QString batimentChoisie = QInputDialog::getText(nullptr, windowJoueur, "Batiment choisie :", QLineEdit::Normal, "", &ok1);
+    for (unsigned int i=0; i<7; i++) {
         for (unsigned int j=1; j<12; j++) {
             if (structure[i][j]!=nullptr && structure[i+1][j-1]==nullptr && structure[i+1][j+1]==nullptr && batimentChoisie.toStdString()==structure[i][j]->getNom()) {
                 Batiment* temp=structure[i][j];
@@ -187,15 +233,14 @@ Merveille PlateauDeJeu::choisirMerveille(bool j2) {
 }
 
 void PlateauDeJeu::setDefausse(Batiment& bat) {
-    
     if( nb_defausse >= nb_max_defausse){
-        Batiment** newdefausse = new Batiment*[nb_max_defausse*2 + 1]; //augmentation même si nb_max_defausse = 0
+        Batiment** newdefausse = new Batiment*[nb_max_defausse*2];
         for(int i = 0; i < nb_defausse; i++){
             newdefausse[i] = defausse[i];
         }
         Batiment **old = defausse;
         defausse = newdefausse;
-        nb_max_defausse = nb_max_defausse * 2 + 1;
+        nb_max_defausse*=2;
         delete[] old;
     }
     defausse[nb_defausse++] = &bat;
@@ -219,7 +264,7 @@ Batiment* PlateauDeJeu::piocher(int i) {
 std::ostream& operator<<(std::ostream& f, const PlateauDeJeu& pla) {
     f << "Merveilles sur le plateau : " << std::endl;
     for (unsigned int i=0; i<pla.getNb_merveille_plateau(); i++) f << pla.getSelectionMerveille()[i] << std::endl;
-    f << "Batiments sur le plateau : " << std::endl;
+    f << "Batiments sur le plateau : " << pla.getNb_batiment_plateau() << std::endl;
     for (unsigned int i=0; i<pla.getNb_batiment_plateau(); i++) f << *(pla.getTab()[i]);
     return f;
 }

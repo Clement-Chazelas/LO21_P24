fb124cc854f8a5de80fc8e2878837d58688a164a
TitouanChevalier <117170107+TitouanChevalier@users.noreply.github.com>
2024-05-16 11:00:16 +0200

    Update To do list

--- main.cpp
+++ main.cpp
@@ -33,14 +33,13 @@ int main(int argc, char *argv[]) {
 
 /*
 To Do list :
-    - Generation age III
-    - Gestion du masquage des batiments
     - Generation aleatoire des jetons scientifique sur et hors plateau
-    - Ajout des methodes joueurs pour calculer le cout d'achat
-    - Ajout des methodes joueurs pour calculer l'argent gagnées par une defausse
     - Finaliser l'ajout des attributs spéciaux des merveilles (ressources disponibles, choix du jeton, choix dans la defausse...
     - Gestion du conflit militaire
-    - mthode pour calculer les points de victoires en parcourant les cartes de la cite
-    ...
+    - Assigner la gestion du cycle de vie de tous les objets chacun à une classe
+    - Nettoyer le code (retirer les methodes inutilises, nettoyer la memoire pendant la partie, commencer à mettre des commentaires pour les méthodes un peu complexes)
+    - faire une méthode dans la classe partie pour faire derouler l'integralite de la partie
+    - Photocopier les cartes du jeu
+    - S'attaquer à la partie graphique...
 */
 

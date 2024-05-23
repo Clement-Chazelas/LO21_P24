d8466d5b7237e188880ecdeebb8986c02151f2df
Clement-Chazelas <154357160+Clement-Chazelas@users.noreply.github.com>
2024-05-23 23:19:06 +0200

    jetonProgres terminé

--- main.cpp
+++ main.cpp
@@ -33,6 +33,41 @@ int main(int argc, char *argv[]) {
 
     p.getSetPlateau().genererJetons();
 
+    std::cout << "Jetons de la pioche avant choix:\n";
+    for (size_t i = 0; i < p.getPlateau().getNb_jeton_pioche(); ++i) {
+        std::cout << p.getPlateau().getJetonProgresPioche()[i] << std::endl;
+    }
+
+    std::cout << "\nJetons sur le plateau avant choix:\n";
+    for (size_t i = 0; i <p.getPlateau().getNb_jeton_plateau(); ++i) {
+        std::cout << p.getPlateau().getJetonProgresPlateau()[i] << std::endl;
+    }
+
+    // Le joueur 1 prend un jeton dans la pioche
+    p.getSetPlateau().prendreJetonDansPioche(p.getSetJoueur1());
+
+    // Le joueur 2 prend un jeton sur le plateau
+    p.getSetPlateau().prendreJetonDansPlateau(p.getSetJoueur2());
+
+    // Affichage des jetons des joueurs
+    std::cout << "\nJetons de progrès du joueur 1 après choix dans la pioche:\n";
+    p.getJoueur1().afficherJetons();
+
+    std::cout << "\nJetons de progrès du joueur 2 après choix sur le plateau:\n";
+    p.getJoueur2().afficherJetons();
+
+    // Affichage des jetons restants dans la pioche et sur le plateau
+    std::cout << "\nJetons de la pioche après choix:\n";
+    for (size_t i = 0; i < p.getPlateau().getNb_jeton_pioche(); ++i) {
+        std::cout << p.getPlateau().getJetonProgresPioche()[i] << std::endl;
+    }
+
+    std::cout << "\nJetons sur le plateau après choix:\n";
+    for (size_t i = 0; i < p.getPlateau().getNb_jeton_plateau(); ++i) {
+        std::cout << p.getPlateau().getJetonProgresPlateau()[i] << std::endl;
+    }
+
+
     return 0;
 }
 

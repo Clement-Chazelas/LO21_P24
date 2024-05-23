919f359185d2fc49baa33dbd48e366092ce5a174
Clement-Chazelas <154357160+Clement-Chazelas@users.noreply.github.com>
2024-05-23 16:47:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ int main(int argc, char *argv[]) {
     Partie p("Ducasse", "Agathe", "Chazelas", "Clement");
     cout << p << endl;
 
-
+    /*
     p.genererHuitMerveilles(); //generation random parmis un csv pour choisir 8 merveilles
     p.selectionDesMerveilles(); //Choix successifs des merveilles
+    */
 
-    /*
-    p.genererAgeUn(); //generation random parmis un csv pour exclure 3 batiments
+    /*p.genererAgeUn(); //generation random parmis un csv pour exclure 3 batiments
     p.getPlateau().genererStructureAge1(); //generation de la structure plateau des batiments de l'age 1
     p.selectionDesBatiments(); //Choix successifs des batiments
 
@@ -29,24 +29,20 @@ int main(int argc, char *argv[]) {
 
     p.genererAgeTrois();
     p.getPlateau().genererStructureAge3();
-    p.selectionDesBatiments();
-    */
+    p.selectionDesBatiments();*/
 
-    p.getPlateau().genererJetons();    //Génération aléatoire des Jetons Progrès
+    p.getSetPlateau().genererJetons();
 
     return 0;
 }
 
 /*
 To Do list :
-    - Finaliser la methode de decompte des points de victoires (Titouan)
-    - Finaliser la methode de decompte des symboles scientifiques (Titouan)
-    - Ecrire la methode pour supprimer une ressource (manufacture ou primaire) a l'adversaire (Agathe)
-    - Ecrire la methode pour choisir un jeton scientifique hors plateau selon les regles du jeu (Clement)
-    - Gestion du conflit militaire (Agathe)
-    - Ecrire Methode de fin de partie (Agathe)
-    - agrandir le CSV pour eviter les exceptions
-    - Nettoyer le code (retirer les methodes inutilises, nettoyer la memoire pendant la partie, commencer à mettre des commentaires pour les méthodes un peu complexes) 
+    - Generation aleatoire des jetons scientifique sur et hors plateau
+    - Finaliser l'ajout des attributs spéciaux des merveilles (ressources disponibles, choix du jeton, choix dans la defausse...
+    - Gestion du conflit militaire
+    - Assigner la gestion du cycle de vie de tous les objets chacun à une classe
+    - Nettoyer le code (retirer les methodes inutilises, nettoyer la memoire pendant la partie, commencer à mettre des commentaires pour les méthodes un peu complexes)
     - faire une méthode dans la classe partie pour faire derouler l'integralite de la partie
     - Photocopier les cartes du jeu
     - S'attaquer à la partie graphique...

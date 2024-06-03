29371ab47657376815109fe1ae32a03544fbfc62
Clement-Chazelas <154357160+Clement-Chazelas@users.noreply.github.com>
2024-06-03 13:30:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,16 @@ int main(int argc, char *argv[])
     w.showMaximized();
     return a.exec();
 }
+
+
+/*To do list
+
+-Faire affichage selection des merveilles
+-Faire affichage Age 1
+-Faire affichage Age 2
+-Faire affichage Age 3
+-Faire des bons grids pour l'interface
+-On reprend les images de Board Game Arena pour le fond
+
+
+*/
